8d9248a97c6f8573d47ff44c1905d8d46ec8d714
nanachiki <nanachiki256@gmail.com>
2025-07-15 17:32:18 +0900

    Display sent and received message timestamps

--- UDP-client.py
+++ UDP-client.py
@@ -1,58 +1,50 @@
 import socket
 import sys
 import os
+from datetime import datetime
+
+
+# Function to get formatted timestamp
+def get_timestamp():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
 
 server_address = '/tmp/udp_socket_file'
-
 address = '/tmp/udp_client_socket_file'
 
-message = input("Enter your message to send to the server: ")
 
 try:
+    # Delete a previous address and bind the new one to the socket
     os.unlink(address)
-except FileNotFoundError:
-    pass
-
-# Bind the client address to the socket
-try:
     sock.bind(address)
-except socket.error as e:
-    print('Socket connection failed: {}'.format(e))
+
+    # Send a message to the server
+    # Create a loop that allows multiple messages without restarting
+    
+    while True:
+        message = input("Enter your message (or 'quit' to exit): ")
+        if message.lower() in ['quit', 'exit', 'q']:
+            print("Goodbye!")
+            break # This exists the while loop
+        
+        timestamp = get_timestamp()
+        print(f'📤 [{timestamp}] Sending to Server: {message}')
+        sent = sock.sendto(message.encode(), server_address)
+
+        # Receive 4096 bytes data at the maximum
+        print('Waiting to receive...')
+
+        data, server = sock.recvfrom(4096)
+
+        # Display the received data from the server
+        timestamp = get_timestamp()
+        print(f'📩 [{timestamp}] Received from Server: {data}')
+
+except Exception as e:
+    print('Error: {}'.format(e))
     sys.exit(1)
 
-# Send a message to the server
-# try:
-# Create a loop that allows multiple messages without restarting
-loop = True
-while loop:
-    print('Sending {!r}'.format(message))
-    # message = message.encode()
-    sent = sock.sendto(message.encode(), server_address)
-
-    # Receive 4096 bytes data at the maximum
-    print('Waiting to receive')
-
-    data, server = sock.recvfrom(4096)
-
-    # Display the received data from the server
-    # data_str = data.decode('utf-8')
-    print('Received message in binary: {!r}'.format(data))
-    # print('Received message in string: {!s}'.format(data))
-    # print('Received message in string: {!a}'.format(data))
-    print('The message is sent from: {!r}'.format(server))
-
-    question = input("Would you like to send another message? Y/N ")
-    while True:
-        if question == "Y":
-            message = input("Enter your message to send to the server: ")
-            break
-        elif question =="N":
-            loop = False
-            break
-        question = input("Would you like to send another message? Y/N ")
-
-# finally:
-#     print('Closing socket')
-#     sock.close()
+finally:
+    print('Closing socket')
+    sock.close()
